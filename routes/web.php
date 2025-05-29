86a40ff2bb2e237cc813e1891a4bae92caded992
anastasyafdg <anastasyaginting565@gmail.com>
2025-05-29 11:42:21 +0700

    Update Praktikum 8

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ListProdukController;
 
 // Route::get('/', function () {
 //   return view('welcome');
@@ -11,3 +12,4 @@ use App\Http\Controllers\ProductController;
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/product', [ProductController::class, 'index']);
+Route::get('/listproduk', [ListProdukController::class, 'show']);
